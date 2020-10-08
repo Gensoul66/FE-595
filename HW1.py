df08d2868e160055df3bfd9b5a8e149d724dc432
Haohang Li <hhl0871@outlook.com>
2020-10-07 23:22:12 -0400

    Add the tangent plot and keep the original y-axis scale

--- HW1.py
+++ HW1.py
@@ -11,6 +11,23 @@ def main():
     # plot them out
     plt.plot(x, y_sine, label="Sine")
     plt.plot(x, y_cosine, label="Cosine")
+
+    plt.plot(np.linspace(0, 0.5 * np.pi),
+             np.tan(np.linspace(0, 0.5 * np.pi)),
+             label='Tangent',
+             color='red')  # added by Haohang Li
+
+    plt.plot(np.linspace(0.5 * np.pi + 1e-10, 1.5 * np.pi),
+             np.tan(np.linspace(0.5 * np.pi + 1e-10, 1.5 * np.pi)),
+             color='red')  # added by Haohang Li
+
+    plt.plot(np.linspace(1.5 * np.pi + 1e-10, 2 * np.pi),
+             np.tan(np.linspace(1.5 * np.pi + 1e-10, 2 * np.pi)),
+             color='red')  # added by Haohang Li
+    
+    plt.axhline(y=0.0, color='black')  # added by Haohang Li
+
+    plt.ylim(-1.00, 1.00)  # the original scale is from -1.0 to 1.0, added by Haohang Li
     plt.xlabel('x - axis')
     plt.ylabel('y-axis')
     plt.title('My lines')
